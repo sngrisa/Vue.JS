414792d5c253049b80fd6e8a7d87633c4c38c3b8
Santiago Grisafi <sngrisa@gmail.com>
2020-11-29 19:09:02 -0300

    Avance con el curso de formularios con Vue.JS

---
+++ sig-games/src/components/navbar/navbar.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'navbar',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
